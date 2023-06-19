e84310e43a8cafa5929a2330fd397d4391f216a3
cjimenezgallardo <carlos.jimenez@ufrontera.cl>
2023-06-19 10:22:15 -0400

    atajos
    para usar en r

--- Librerias.R
+++ Librerias.R
@@ -1,76 +1,74 @@
 #LIBRERIAS
-library(actuar) # analisis de distribuciones con colas importantes..
-library(AER) #herramientas regresion y econometria
-library(agricolae)
-library(arm) #analisis de datos con regresion
 
-library(car) # regresion
-library(caret) #by, bootstrapp
 
-library(DescTools) #estadistica en general
-library(devtools) #para desarrollo de paquetes
-library(doBy) #Estadisticas agrupadas
+# NECESARIA VARIOS PROCESOS DATOS, REGRESION COMPARACIONES
+library(caret) #by, bootstrapp y otros
 library(dplyr) #manejo de dataframes
-
-
-library(e1071) # funciones de probabilidad
-library(EnvStats) # informacion estadistica mas completa
-library(evir) # valores extremos
-library(evmix) # valores extremos
-
-library(fitdistrplus) # funciones de probabilidad
-library(forcats)
-library(foreign) #lectura datos distintas fuentes
-library(fdth)  # Crear tablas de distribuci?n
-
-
-
-library(ISLR) # Para regresion 
-
-library(knitr) # Tablas amigables
-
-
-library(lmtest) # test de analisis de regresion
-library(lubridate) 
-
-library(MASS) # analisis estadisstico mas avanzado
-library(moments) #estadistica
-library(multcomp) # visualizacion de comparaciones multiples
-library(multcompView) # visualizacion de comparaciones multiples
-
-library(naniar) 
 library(nortest) #pruebas de normalidad
-
-library(outliers) #para identificacion y trabajo con outliers
-library(onewaytests) # para pruebas POST HOC
-
-library(PerformanceAnalytics) # Analisis de Riesgo y Regresion
-library(PMCMRplus) #test
-library(ppcor) #correlaciones parciales y semi
-library(psych) #procedimientos psicometricos y estadisticos
-
-library(roxygen2) #documentacion 
-
-library(tables)
 library(tidyverse)#anexo manejo de datos
 library(tibble)
 library(tidyr) #trabajo con datos
+library(lubridate) #manejor de datos
 
 
 
-library(writexl)# escritura a XLSX
-
-
-library(samplingbook)
 
 
 
-library(pastecs)
-library(summarytools)
-library(DataExplorer)
-library(Rmisc)
 
-# grafica
+#EXPLORACION DE DATOS
+library(DescTools) #estadistica en general
+library(devtools) #para desarrollo de paquetes
+library(doBy) #Estadisticas agrupadas
+library(EnvStats) # informacion estadistica mas completa
+library(evir) # valores extremos
+library(evmix) # valores extremos
+library(moments) #estadistica
+library(tables)
+library(knitr) # Tablas amigables
+library(MASS) # analisis estadisstico mas avanzado
+library(mosaic) #estadisticas en general
+
+
+
+
+
+
+# PROBABILIDAD Y DATOS
+library(actuar) # analisis de distribuciones con colas importantes..
+library(e1071) # funciones de probabilidad
+library(fitdistrplus) # funciones de probabilidad
+library(gamlss)
+
+
+
+# REGRESION
+library(AER) #herramientas regresion y econometria
+library(arm) #analisis de datos con regresion
+library(ISLR) # Para regresion 
+library(car) # regresion
+library(PerformanceAnalytics) # Analisis de Riesgo y Regresion
+library(lmtest) # test de analisis de regresion
+library(ggfortify)
+library(gvlma)
+
+
+
+
+# INFERENCIA
+library(agricolae)
+library(onewaytests) # para pruebas POST HOC
+library(PMCMRplus) #test
+library(ppcor) #correlaciones parciales y semi
+library(psych) #procedimientos psicometricos y estadisticos
+library(lawstat)
+library(lsr)
+library(effsize)
+
+
+
+
+# GRAFICA
 library(xts)
 library(dygraphs)
 library(gganimate)
@@ -86,5 +84,46 @@ library(highchart) #para graficar
 library(hrbrthemes)
 library(viridis) #grafica
 library(lattice) # graficas avanzada
+library(gdtools) #para grafica
+
+
+
+#para escribir en formatos "REVISTAS"
+library(apa)
+
+
+
+
+
+#pendientes por ordenar
+library(forcats)
+library(foreign) #lectura datos distintas fuentes
+library(fdth)  # Crear tablas de distribuci?n
+library(multcomp) # visualizacion de comparaciones multiples
+library(multcompView) # visualizacion de comparaciones multiples
+library(naniar) 
+
+
+library(outliers) #para identificacion y trabajo con outliers
+
+
+library(roxygen2) #documentacion 
+library(rsample) #muestreo y bootstrapping
+
+
+
+
+
+library(writexl)# escritura a XLSX
+
+
+library(samplingbook)
+library(pastecs)
+library(summarytools)
+library(DataExplorer)
+library(Rmisc)
+
+# grafica
+
 
 
---
+++ atajos de teclado.R
@@ -0,0 +1,64 @@
+# atajos con teclado
+
+
+#Ctrl (+) Enter = Run
+
+#Alt (+) enter = Run pero sin avanzar el cursor de línea de código.
+
+#Ctrl (+) Shift (+) M =  %>%   
+
+#Alt (+) - = <-
+
+#Ctrl (+) F = buscar y remplazar (mac = ⌘F)
+
+#Ctrl (+) Shift (+) F = buscar archivos (de sintaxis por ejemplo) (mac = ⇧⌘F)
+
+#Ctrl (+) . = te busca una función dentro del código (mac = ⌃.)
+
+#Ctrl (+) Shift (+) A = introducir una nueva línea de código  (mac = ⇧⌘A)
+
+#Ctrl (+) D = eliminar una línea de código (mac = ⌘D)
+
+#Ctrl(+)F11/F12 (con la tecla de fn seguramente) para cambiar de pestaña
+#lo mismo Alt (+) ctrl (+) flecha derecha o izquierda (mac = ⌃F11 o ⌃⌥←)
+
+#Ctrl(+)p = te lleva de un paréntesis a su propio final (qué triste) (mac = ⌃P)
+
+
+#alt (+) up/down: mueve el código seleccionado arriba o abajo (mac = ⌥↑ or ↓)
+#alt (+) shift (+) up/down: copia el código seleccionado arriba o abajo (mac = ⌥⌘↑ or ↓)#alt (+) shift (+) up/down: copia el código seleccionado arriba o abajo (mac = ⌥⌘↑ or ↓)
+
+#ctrl (+) (+) = acerca el zoom 
+#ctrl (+) - aleja el zoom
+
+#Alt (+) drag = múltiples cursores
+
+#Ctrl (+) L = limpiar la consola  (mac = ⌃L)
+
+#Ctrl (+) Shift (+) 1 = la ventana se centra en la sintaxis y la maximiza (mac = ⌃⇧1)
+#Ctrl (+) Shift (+) (+) = la ventana se centra en el ambiente y lo maximiza (mac = ⌃⇧8)
+#Ctrl (+) Shift (+) 6 = la ventana se centra en los plots y los maximiza (mac = ⌃⇧6)
+#Ctrl (+) Shift (+) Alt (+) 0 = vuelta a la normalidad. 
+
+#Ctrl (+) Shift (+) flecha arriba = seleccionar todo el código de arriba hasta que encuentre una línea vacia (mac = ⌃⌥⇧↑)
+#Ctrl (+) Shift (+) flecha arriba (+) flecha arriba  = todo lo de arriba
+#lo mismo con la flecha para abajo.
+#Ctrl (+) A = seleccionar todo el código (mac = ⌘A)
+
+#Ctrl (+) Alt (+) Shift (+) M = renombra variables en el propio script (mac = ⌥⇧⌘M)
+
+coche 
+
+coche 
+
+coche 
+
+#Ctrl (+) 1 o 2 = para oscilar entre el código y la consola. (mac  = ⌃1 o ⌃2)
+
+#Ctrl (+) Alt (+) B = runea todo el código hasta donde estes seleccionando (mac =  ⌥⌘B)
+
+#Ctrl (+) Shift (+) R = poner una etiqueta (tidy) (mac = ⇧⌘R or ⌃⇧R)
+
+# analísis 3 --------------------------------------------------------------
+
+#alt (+) shift (+) K = te muestra todos los atajos del teclado (mac = ⌥⇧K)
