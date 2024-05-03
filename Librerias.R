2f1b38a0cd33089564b341a27f8ad7dbcace9d16
cjimenezgallardo <carlos.jimenez@ufrontera.cl>
2024-05-03 14:55:44 -0400

    Actualizacion
    se agregar VGAM, distribuciones de probabilidad
    se eliminan las algunas. por antiguedad.

--- Librerias.R
+++ Librerias.R
@@ -1,14 +1,27 @@
 #LIBRERIAS
 
 
-# NECESARIA VARIOS PROCESOS DATOS, REGRESION COMPARACIONES
+# siempre cargar 
+library(tidyverse)#anexo manejo de datos 
 library(caret) #by, bootstrapp y otros
-library(dplyr) #manejo de dataframes
-library(nortest) #pruebas de normalidad
-library(tidyverse)#anexo manejo de datos
-library(tibble)
+library(tibble) #dataframes simples para mejorar presentacion con broom
 library(tidyr) #trabajo con datos
+library(broom) #convierte resultados en formatos mejor vistos
 library(lubridate) #manejor de datos
+library(haven) #importa datos desde SPSS, SAS; STATA
+
+
+
+
+#tablas
+library(report) # genera informe de resultados estadisticos
+library(expss) # tablas etiquetas y otras funciones
+library(kableExtra) # construccion de tablas avanzadas.
+library(rempsyc) #tablas con formato 
+library(flextable)
+library(broom)
+library(xtable)
+library(tableone)
 
 
 
@@ -16,7 +29,7 @@ library(lubridate) #manejor de datos
 
 
 
-#EXPLORACION DE DATOS
+#EXPLORACION DE DATOS 
 library(DescTools) #estadistica en general
 library(devtools) #para desarrollo de paquetes
 library(doBy) #Estadisticas agrupadas
@@ -39,6 +52,9 @@ library(actuar) # analisis de distribuciones con colas importantes..
 library(e1071) # funciones de probabilidad
 library(fitdistrplus) # funciones de probabilidad
 library(gamlss)
+library(nortest) #pruebas de normalidad
+library(VGAM) # Distribuciones de probabilidad
+
 
 
 
@@ -51,19 +67,37 @@ library(PerformanceAnalytics) # Analisis de Riesgo y Regresion
 library(lmtest) # test de analisis de regresion
 library(ggfortify)
 library(gvlma)
+library(forestmodel)
+library(jtools)
+library(olsrr)
+
+
+library(plot3D) 
+library(plot3Drgl)
+library(apaTables)
+library(gvlma)
+library(performance)
+
 
 
 
 
 # INFERENCIA
 library(agricolae)
+library(pwr) # analisis de potencia de pruebas estadisticas
 library(onewaytests) # para pruebas POST HOC
-library(PMCMRplus) #test
+library(PMCMRplus) #test POST HOC T2 T3 entre otros
 library(ppcor) #correlaciones parciales y semi
 library(psych) #procedimientos psicometricos y estadisticos
 library(lawstat)
 library(lsr)
 library(effsize)
+library(effectsize)
+library(nortest) #pruebas de normalidad
+library(rstatix) #pruebas estadisticas
+library(FSA) # comparaciones multiples 
+
+
 
 
 
@@ -73,19 +107,18 @@ library(xts)
 library(dygraphs)
 library(gganimate)
 library(plotly)
-library(chorddiag)
+
 library(esquisse) # para grafica dinamica
 library(GGally) #grafica
 library(ggthemes) #grafica
 library(ggThemeAssist) #grafica
 library(gdtools) #grafica
 library(ggplot2) #mejoras de graficos
-library(highchart) #para graficar
 library(hrbrthemes)
 library(viridis) #grafica
 library(lattice) # graficas avanzada
 library(gdtools) #para grafica
-
+library(highcharter) # mejorar grafica
 
 
 #para escribir en formatos "REVISTAS"
@@ -124,6 +157,27 @@ library(DataExplorer)
 library(Rmisc)
 
 # grafica
+
+
+
+# para cargar múltiple librerias
+
+carga_lib <- function(librerias){
+  a_cargar <- librerias[!(librerias %in% installed.packages()[, "Package"])]
+  if (length(a_cargar)) 
+    install.packages(a_cargar, 
+                     dependencies = TRUE)
+  sapply(librerias, 
+         character.only = TRUE, 
+         require)
+}
+
+#define las librerias que necesitas
+librerias <- c("tidyverse",
+               "PerformanceAnalytics")
+
+#carga las librerias 
+carga_lib(librerias)
 
 
 
